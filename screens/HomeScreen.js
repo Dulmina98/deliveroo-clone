570fa325c7fe0b504dc64025f7d1ae69ed526745
Dulmina98 <dulminawijegunarathne@gmail.com>
2023-04-04 09:48:09 +0530

    Configure tailwind css, react navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+
+export function HomeScreen() {
+
+    return (
+        <View>
+            <Text className={'text-red-500'}>Home Screen</Text>
+        </View>
+    )
+}
